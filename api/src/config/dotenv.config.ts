f685da23f6c0974ffb9322b69460cc28f37539e0
lucasmonteiroi <lmsupport@outlook.com>
2020-06-11 01:21:02 -0300

    fix: env configurations

--- api/src/config/dotenv.config.ts
+++ api/src/config/dotenv.config.ts
@@ -6,6 +6,12 @@ switch (process.env.NODE_ENV) {
   case 'production':
     _path = `${__dirname}/../../.env.production`;
     break;
+  case 'docker':
+    _path = `${__dirname}/../../.env.production`;
+    break;
+  case 'homolog':
+    _path = `${__dirname}/../../.env.test`;
+    break;
   default:
     _path = `${__dirname}/../../.env.development`;
 }
